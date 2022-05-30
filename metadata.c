4f6e999b84a1611e0e56ab92c0916709e6c4f87d
Eric Toombs <not@public.net>
2022-05-30 18:08:30 -0400

    Added opus support
    The changes are identical to the patch provided in https://sourceforge.net/p/minidlna/patches/133/, but without changing the existing behaviour of ogg. That can happen on a different commit.

--- metadata.c
+++ metadata.c
@@ -308,6 +308,11 @@ GetAudioMetadata(const char *path, const char *name)
 		strcpy(type, "ogg");
 		m.mime = strdup("audio/ogg");
 	}
+	else if ( ends_with(path, ".opus") )
+	{
+		strcpy(type,"ops");
+		m.mime = strdup("audio/ogg; codecs=opus");
+	}
 	else if( ends_with(path, ".pcm") )
 	{
 		strcpy(type, "pcm");
